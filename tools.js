3299539feb0bfa64ec9f27850a634fce005b6c67
Nick Frichette <frichetten@gmail.com>
2018-03-09 10:08:39 -0600

    Added the html folder to the gitignore, don't want to risk someone pushing a cloned website to a repo. Could cause problems

--- tools.js
+++ tools.js
@@ -3,7 +3,15 @@ const ui = require('./user_interface')
 module.exports = {
    clone_a_website: function(){
      ui.prompt("Target URL: ", function(input) {
-       console.log(input);
+       const util = require('util');
+       const exec = util.promisify(require('child_process').exec);
+       async function ls() {
+         const { stdout, stderr } = await exec(
+		 'mkdir ./html/' + input + ' && cd ./html/' + input + ' && httrack --footer "" ' + input);
+	 console.log('stdout:',stdout);
+	 console.log('stderr:',stderr);
+       }
+       ls();
      });
    }
 };
